9bcbc0fb055400473915b0999309604b30bb0929
Daigo Shitara <shitara@tagbangers.co.jp>
2024-07-26 11:06:04 +0900

    exercises

---
+++ language_list.ex
@@ -0,0 +1,25 @@
+defmodule LanguageList do
+  def new() do
+    []
+  end
+
+  def add(list, language) do
+    [language | list]
+  end
+
+  def remove(list) do
+    list |> tl
+  end
+
+  def first(list) do
+    list |> hd
+  end
+
+  def count(list) do
+    list |> length
+  end
+
+  def functional_list?(list) do
+    "Elixir" in list
+  end
+end
